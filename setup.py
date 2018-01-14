b3a2d84f9fd7a4c04499a8abdcba1770307c157e
Michael McEniry [UAH] <mcenirm@uah.edu>
2018-01-14 10:45:14 -0600

    Add local version for HTMLParser fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="htmldiff",
-    version="1.0.0.dev6",
+    version="1.0.0.dev6+six.htmlparser",
     author="Ian Bicking - https://github.com/ianb",
     description=("Utility to create html diffs"),
     packages=find_packages('src'),
