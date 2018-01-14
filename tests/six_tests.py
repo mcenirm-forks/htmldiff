e589c61f9f0090b099dd10b009f376d0a44e3eb8
Michael McEniry [UAH] <mcenirm@uah.edu>
2018-01-14 10:33:06 -0600

    Boilerplate for "six" tests

---
+++ tests/six_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class SixTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_six_import(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
